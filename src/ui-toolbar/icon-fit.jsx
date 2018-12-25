8d971259aa2289cce8705c2d42e797bb73c175bb
iminyukov <38067654+iminyukov@users.noreply.github.com>
2018-12-25 17:04:27 +0300

    Update icon-fit.jsx

--- src/ui-toolbar/icon-fit.jsx
+++ src/ui-toolbar/icon-fit.jsx
@@ -6,7 +6,7 @@ export default function IconFit() {
   return (
     <svg width={24} height={24} stroke="currentColor">
       <path
-        d="M15 3l2.3 2.3-2.89 2.87 1.42 1.42L18.7 6.7 21 9V3zM3 9l2.3-2.3 2.87 2.89 1.42-1.42L6.7 5.3 9 3H3zm6 12l-2.3-2.3 2.89-2.87-1.42-1.42L5.3 17.3 3 15v6zm12-6l-2.3 2.3-2.87-2.89-1.42 1.42 2.89 2.87L15 21h6z"/>
+        d="M5,5H10V7H7V10H5V5M14,5H19V10H17V7H14V5M17,14H19V19H14V17H17V14M10,17V19H5V14H7V17H10Z"/>
     </svg>
   );
 }
